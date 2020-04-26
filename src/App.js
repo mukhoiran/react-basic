2f0a0daac68056932396f53ca0350c44519caf35
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-26 21:46:36 +0700

    component and props

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//note :
+// - component like function
+// - props like parameter
+
+//new component
+// function Greeting(){
+//   return <h1> Hello Messi </h1>
+// }
+
+//new component with props
+function Greeting(props){
+  // return <h1> Hello {props.name}, {props.age} </h1>
+  return <h1> Hello {props.name}, your number is <Bio number={props.number}/> </h1>
+}
+
+function Bio(props){
+  return <span> {props.number} </span>
+}
+
+//default component
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        {/* call component */}
+        {/* <Greeting/> */}
+
+        {/* call component with props*/}
+        <Greeting name="Suarez" number="9"/>
+        <Greeting name="Griezmann" number="17"/>
+
+    </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -11,19 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const name = 'Lionel Messi';
-// const element = <h1>Hello, {name} !!</h1>;
-
-function greet(){
-  return "How are you " + name
-}
-
-const element = <h1>Hello, {greet()} ?</h1>;
-
-ReactDOM.render(element,
-  document.getElementById('master')
-);
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
