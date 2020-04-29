251735165a4fdb1a65f1a5640fc52c677be1e26c
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-29 10:09:40 +0700

    state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -21,6 +21,21 @@ function Bio(props){
   return <span> {props.number} </span>
 }
 
+//component state
+function Timer(){
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  )
+}
+
 //default component
 function App() {
   return (
@@ -32,9 +47,12 @@ function App() {
         {/* <Greeting/> */}
 
         {/* call component with props*/}
-        <Greeting name="Suarez" number="9"/>
-        <Greeting name="Griezmann" number="17"/>
+        {/*
+          <Greeting name="Suarez" number="9"/>
+          <Greeting name="Griezmann" number="17"/>
+        */}
 
+        <Timer/>
     </header>
     </div>
   );
